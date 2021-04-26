f142c167c7994004ac757ec8a424168d6cd62cd6
BioWork <bigjarik@gmail.com>
2021-04-26 18:39:15 +0300

    Добавьте файлы проекта.

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace tink_oblig
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.keypare_cmb = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(12, 41);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(67, 22);
+            this.login_btn.TabIndex = 0;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = true;
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(268, 41);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(67, 22);
+            this.exit_btn.TabIndex = 0;
+            this.exit_btn.Text = "Exit";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            // 
+            // keypare_cmb
+            // 
+            this.keypare_cmb.FormattingEnabled = true;
+            this.keypare_cmb.Location = new System.Drawing.Point(12, 12);
+            this.keypare_cmb.Name = "keypare_cmb";
+            this.keypare_cmb.Size = new System.Drawing.Size(323, 23);
+            this.keypare_cmb.TabIndex = 1;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.login_btn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exit_btn;
+            this.ClientSize = new System.Drawing.Size(347, 72);
+            this.ControlBox = false;
+            this.Controls.Add(this.keypare_cmb);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.login_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.ComboBox keypare_cmb;
+    }
+}
+
---
+++ classes/Bounds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Tinkoff.Trading.OpenApi.Models;
+
+namespace tink_oblig.classes
+{
+    public class Bound : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Tinkoff Position Base Class
+        /// </summary>
+        public Portfolio.Position Base { get; set; }
+        public Bound(Portfolio.Position ps)
+        {
+            Base = ps;
+        }
+
+        #region NoTinkoffInfo
+        //искать в инфо о бумаге
+        public decimal Nominal { get; set; }
+        public DateTime Prev_pay_dt { get { return Next_pay_dt.AddDays(-Pay_period); } }
+        public DateTime Next_pay_dt { get; set; }
+        public DateTime End_pay_dt { get; set; }
+        public int Pay_period { get; set; }
+        public decimal Cpn_val { get; set; }
+        public decimal Cpn_Percent { get; set; }
+
+        //искать в истории
+        public List<decimal> Payed_cpn_list { get; set; }
+        public decimal Total_payed { get { return Payed_cpn_list.Sum(); } }
+        public int Total_payed_cnt { get; set; }
+        public decimal Total_cash_back { get; set; }
+        #endregion  
+    }
+}
